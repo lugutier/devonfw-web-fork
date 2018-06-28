5a46b1c3f79e749950edb2c17f944e86ebc84ded
lugutier <39942939+lugutier@users.noreply.github.com>
2018-06-28 16:22:46 +0200

    Lot of changes: Modular architecture implemented. Intranet navbar still unconfigured folder and archives moving

--- dist/javascript/main.js
+++ dist/javascript/main.js
@@ -1,14 +1,10 @@
-function ping() {
-    /*$.ajax({
-        url: 'https://troom.capgemini.com/sites/vcc/devon/overview.aspx',
-        success: function (result) {
-            alert('reply');
-        },
-        error: function (result) {
-            alert('timeout/error');
-        }
-    });*/
+$(document).ready(function () {
+    $("insertHtml").each(function (i) {
+        $(this).load($(this).attr("url"));
+    });
+});
 
+function ping() {
     $.ajax({
         url: 'https://troom.capgemini.com/sites/vcc/devon/overview.aspx',
         dataType: 'jsonp',
