2751b7ce058998e33574f236e158d13f573b3efa
CORP\landress <laura.andres-sauco@capgemini.com>
2018-07-02 13:42:58 +0200

    responsive navmenu - subitems need to be fixed

--- dist/javascript/main.js
+++ dist/javascript/main.js
@@ -1,27 +1,25 @@
-$(document).ready(function () {
-    $("insertHtml").each(function (i) {
-        $(this).load($(this).attr("url"));
-    });
+$(document).ready(function() {
+  $('insertHtml').each(function(i) {
+    $(this).load($(this).attr('url'));
+  });
 });
 
-function ping() {
-    $.ajax({
-        url: 'https://troom.capgemini.com/sites/vcc/devon/overview.aspx',
-        dataType: 'jsonp',
-        method: 'GET',
-        timeout: 2000,
-        error: function (hrx, textStatus, error) {
-            if (textStatus === 'parsererror') {
-                //$('.cg_internal').show();
-                //$('.extern').hide();
-                /*$('[data-cap-href]').each(function () {
-                    $(this).attr('href', $(this).data('cap-href'));
-                });*/
-                alert('reply');
-            } else {
-                //$('.extern').show();
-                alert('timeout/error');
-            }
-        }
-    });
+function validate_network() {
+  $.ajax({
+    url: 'https://troom.capgemini.com/sites/vcc/devon/overview.aspx',
+    dataType: 'jsonp',
+    method: 'GET',
+    timeout: 2000,
+    error: function(hrx, textStatus, error) {
+      if (textStatus === 'parsererror') {
+        $('.c_internal').show();
+        $('.c_external').hide();
+        $('[data-cap-href]').each(function() {
+          $(this).attr('href', $(this).data('cap-href'));
+        });
+      } else {
+        $('.c_external').show();
+      }
+    },
+  });
 }
