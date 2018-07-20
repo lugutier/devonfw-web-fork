9f584cb0eee9f0fa8fe974998045b5f78f586f4a
Marcel <marcel.a.ochs@capgemini.com>
2018-07-20 10:20:37 +0200

    Added Markdown support

--- dist/javascript/main.js
+++ dist/javascript/main.js
@@ -5,14 +5,28 @@ $(document).ready(function () {
 
 
 function loadInsertions() {
+    console.log("loadInsertions");
     $("insertHtml").each(function (i) {
         let url = $(this).attr("url");
-        $(this).load(url.concat("/component.html"));
+        $(this).load(url.concat("/component.html"), function() {
+            loadMarkdownInsertions();
+        });
         $("<link/>", {
             rel: "stylesheet",
             type: "text/css",
             href: url.concat("/component.css")
         }).appendTo("head");
+
+        console.log("Appending in loadInsertions");
+    });
+}
+
+function loadMarkdownInsertions() {
+    var md = window.mdit;
+    $("insertMd").each(function (i) {
+        fetch($(this).attr("src"))
+        .then(response => response.text())
+        .then(text => $(this).empty().append(md.render(text)))
     });
 }
 
