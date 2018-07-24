15196b77f71d4bd36be98b6bf6f4271bae47cc0a
lugutier <39942939+lugutier@users.noreply.github.com>
2018-07-24 11:58:18 +0200

    architecture improvements

--- dist/javascript/main.js
+++ dist/javascript/main.js
@@ -1,15 +1,16 @@
 $(document).ready(function () {
-    loadInsertions();
-    hideAndShowNetworkElements();
+    loadInsertions($("body"));
 });
 
-
-function loadInsertions() {
-    $("insertHtml").each(function (i) {
+function loadInsertions(view) {
+    view.find("insertHtml").each(function (i) {
         let url = $(this).attr("url");
-        $(this).load(url.concat("/component.html"), function() {
-            loadMarkdownInsertions();
-            loadAsciidocInsertions();
+        $(this).load(url.concat("/component.html"), function () {
+            loadInsertions($(this));
+            loadMarkdownInsertions($(this));
+            loadAsciidocInsertions($(this));
+            hideAndShowNetworkElements($(this));
+            initTooltips($(this));
         });
         $("<link/>", {
             rel: "stylesheet",
@@ -19,25 +20,25 @@ function loadInsertions() {
     });
 }
 
-function loadMarkdownInsertions() {
+function loadMarkdownInsertions(view) {
     var md = window.mdit;
-    $("insertMd").each(function (i) {
+    view.find("insertMd").each(function (i) {
         fetch($(this).attr("src"))
-        .then(response => response.text())
-        .then(text => $(this).empty().append(md.render(text)))
+            .then(response => response.text())
+            .then(text => $(this).empty().append(md.render(text)))
     });
 }
 
-function loadAsciidocInsertions() {
+function loadAsciidocInsertions(view) {
     var ad = window.ad;
-    $("insertAd").each(function (i) {
+    view.find("insertAd").each(function (i) {
         fetch($(this).attr("src"))
-        .then(response => response.text())
-        .then(text => $(this).empty().append(ad.convert(text)))
+            .then(response => response.text())
+            .then(text => $(this).empty().append(ad.convert(text)))
     });
 }
 
-function hideAndShowNetworkElements() {
+function hideAndShowNetworkElements(view) {
     $.ajax({
         url: 'https://troom.capgemini.com/sites/vcc/devon/overview.aspx',
         dataType: 'jsonp',
@@ -53,4 +54,8 @@ function hideAndShowNetworkElements() {
             }
         }
     });
+}
+
+function initTooltips(view) {
+    view.find('[data-toggle="tooltip"]').tooltip();
 }
